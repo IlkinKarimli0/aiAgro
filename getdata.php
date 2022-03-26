0067be575ff2e7a8fd7473992189328930ad68d5
alasgarlikamal <98516464+alasgarlikamal@users.noreply.github.com>
2022-03-26 19:37:20 +0400

    Delete getdata.php

--- getdata.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $servername = "mysql-aiagro.alwaysdata.net";
-    $username = "aiagro_user1";
-    $password = "Kamal04inci06";
-    $dbname = "aiagro_hackathon";
-
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-?>
